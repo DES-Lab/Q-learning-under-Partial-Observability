e47d07189ee835233f79a610740764c9b5000041
Martin Tappler <martin.tappler@ist.tugraz.at>
2022-03-18 12:41:03 +0100

    indicate_wall, no rewards

--- world_repository.py
+++ world_repository.py
@@ -9,6 +9,7 @@ def get_world(world_id):
                        world_file_path='worlds/confusing_big_gravity.txt',
                        is_partially_obs=True,
                        one_time_rewards=False,
+                       indicate_wall=True,
                        max_ep_len=100,
                        goal_reward=10,
                        step_penalty=0.1)
@@ -17,6 +18,7 @@ def get_world(world_id):
                        world_file_path='worlds/big_gravity_2.txt',
                        is_partially_obs=True,
                        one_time_rewards=False,
+                       indicate_wall=True,
                        max_ep_len=100,
                        goal_reward=10,
                        step_penalty=0.2)
@@ -24,7 +26,8 @@ def get_world(world_id):
         env = gym.make(id='poge-v1',
                        world_file_path='worlds/corridor.txt',
                        is_partially_obs=True,
-                       one_time_rewards=True,
+                       one_time_rewards=False,
+                       indicate_wall=True,
                        max_ep_len=100,
                        goal_reward=100,
                        step_penalty=0.2)
@@ -32,8 +35,9 @@ def get_world(world_id):
         env = gym.make(id='poge-v1',
                        world_file_path='worlds/office_world1.txt',
                        is_partially_obs=True,
-                       one_time_rewards=True,
-                       max_ep_len=100,
+                       one_time_rewards=False,
+                       indicate_wall=True,
+                       max_ep_len=200,
                        goal_reward=100,
                        step_penalty=0.2)
     if world_id == 'world1':
@@ -41,6 +45,7 @@ def get_world(world_id):
                        world_file_path='worlds/world1.txt',
                        is_partially_obs=True,
                        one_time_rewards=False,
+                       indicate_wall=True,
                        max_ep_len=100,
                        goal_reward=10,
                        step_penalty=0.1)
@@ -49,7 +54,8 @@ def get_world(world_id):
                        world_file_path='worlds/world2-reward.txt',
                        is_partially_obs=True,
                        one_time_rewards=False,
-                       max_ep_len=400,
+                       indicate_wall=True,
+                       max_ep_len=250,
                        goal_reward=100,
                        step_penalty=2)
     if world_id == 'world2+rew':
@@ -57,6 +63,7 @@ def get_world(world_id):
                        world_file_path='worlds/world2-reward.txt',
                        is_partially_obs=True,
                        one_time_rewards=False,
+                       indicate_wall=True,
                        max_ep_len=200,
                        goal_reward=100,
                        step_penalty=2)
