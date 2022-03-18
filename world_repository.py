2a1d256b087af0f8ca04d5729f9a51d10384b195
Martin Tappler <martin.tappler@ist.tugraz.at>
2022-03-18 14:56:24 +0100

    misleading_office_world.txt fetched from master

--- world_repository.py
+++ world_repository.py
@@ -9,7 +9,6 @@ def get_world(world_id):
                        world_file_path='worlds/confusing_big_gravity.txt',
                        is_partially_obs=True,
                        one_time_rewards=False,
-                       indicate_wall=True,
                        max_ep_len=100,
                        goal_reward=10,
                        step_penalty=0.1)
@@ -18,7 +17,6 @@ def get_world(world_id):
                        world_file_path='worlds/big_gravity_2.txt',
                        is_partially_obs=True,
                        one_time_rewards=False,
-                       indicate_wall=True,
                        max_ep_len=100,
                        goal_reward=10,
                        step_penalty=0.2)
@@ -26,7 +24,7 @@ def get_world(world_id):
         env = gym.make(id='poge-v1',
                        world_file_path='worlds/corridor.txt',
                        is_partially_obs=True,
-                       one_time_rewards=True,
+                       one_time_rewards=False,
                        indicate_wall=True,
                        max_ep_len=100,
                        goal_reward=100,
@@ -37,24 +35,23 @@ def get_world(world_id):
                        is_partially_obs=True,
                        one_time_rewards=False,
                        indicate_wall=True,
-                       max_ep_len=200,
+                       max_ep_len=100,
                        goal_reward=100,
                        step_penalty=0.2)
+    if world_id == 'misleading_office':
+        env = gym.make(id='poge-v1',
+                       world_file_path='worlds/misleading_office_world.txt',
+                       is_partially_obs=True,
+                       one_time_rewards=False,
+                       indicate_wall=True,
+                       max_ep_len=250,
+                       goal_reward=100,
+                       step_penalty=1)
     if world_id == 'world1':
         env = gym.make(id='poge-v1',
                        world_file_path='worlds/world1.txt',
                        is_partially_obs=True,
                        one_time_rewards=False,
-                       indicate_wall=True,
-                       max_ep_len=100,
-                       goal_reward=10,
-                       step_penalty=0.1)
-    if world_id == 'world1+rew':
-        env = gym.make(id='poge-v1',
-                       world_file_path='worlds/world1+rew.txt',
-                       is_partially_obs=True,
-                       one_time_rewards=False,
-                       indicate_wall=True,
                        max_ep_len=100,
                        goal_reward=10,
                        step_penalty=0.1)
@@ -63,8 +60,7 @@ def get_world(world_id):
                        world_file_path='worlds/world2-reward.txt',
                        is_partially_obs=True,
                        one_time_rewards=False,
-                       indicate_wall=True,
-                       max_ep_len=250,
+                       max_ep_len=400,
                        goal_reward=100,
                        step_penalty=2)
     if world_id == 'world2+rew':
@@ -72,7 +68,6 @@ def get_world(world_id):
                        world_file_path='worlds/world2-reward.txt',
                        is_partially_obs=True,
                        one_time_rewards=False,
-                       indicate_wall=True,
                        max_ep_len=200,
                        goal_reward=100,
                        step_penalty=2)
