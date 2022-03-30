dc7fbb0134562aa935212a132ccf7c28d71c3047
Edi Muskardin <edi.muskardin@silicon-austria.com>
2022-03-30 13:36:36 +0200

    paper experiment setup

--- experiment_runner.py
+++ experiment_runner.py
@@ -5,15 +5,16 @@ from reccurent_policy_comp import lstm_experiment
 from stacked_frames_comp import stacked_experiment
 from world_repository import get_world, get_all_world_ids
 
-world_ids = get_all_world_ids()
-
+# world_ids = get_all_world_ids()
+world_ids = ['world1', 'world1_confusing', 'simple_showcase2', 'gravity2']
 # repeat each experiment repeat_each times
-repeat_each = 5
+repeat_each = 2
 # print additional info during each experiment
 verbose = False
 # put to 0.98 if you want to stop when goal is reached in 98% of test episodes
-early_stopping_acc = 1.01
-
+early_stopping_acc = 1.0
+#
+num_ep = 20000
 
 def run_poql_experiments():
     num_runs = len(world_ids) * repeat_each * 2
@@ -28,7 +29,6 @@ def run_poql_experiments():
 
 def run_stacked_experiments():
     algs = [DQN, A2C, ACKTR]
-    repeat_each = 5
     num_runs = len(algs) * len(world_ids) * repeat_each  # * 2 # for frame size of 5 and 10
     i = 0
     frame_size = 5
@@ -38,15 +38,14 @@ def run_stacked_experiments():
             for _ in range(repeat_each):
                 poge = get_world(w)
                 # 30k episodes as default, should be more than enough for all then cut in statistics
-                stacked_experiment(w, poge, alg, 30000 * poge.max_ep_len, num_frames=frame_size,
+                stacked_experiment(w, poge, alg, num_ep * poge.max_ep_len, num_frames=frame_size,
                                    early_stopping_acc=early_stopping_acc, verbose=verbose)
                 i += 1
-                print(f"Percentage of complete Stacked experiments: {round(i / num_runs, 2)}%")
+                print(f"Percentage of complete Stacked experiments: {round(i / num_runs, 2) * 100}%")
 
 
 def run_lstm_experiments():
-    algs = [A2C, ACER, PPO2, ACKTR]
-    repeat_each = 5
+    algs = [ACER, A2C, ACKTR] # PPO2
     num_runs = len(algs) * len(world_ids) * repeat_each
     i = 0
     for w in world_ids:
@@ -54,13 +53,13 @@ def run_lstm_experiments():
             for _ in range(repeat_each):
                 poge = get_world(w)
                 # 30k episodes as default, should be more than enough for all then cut in statistics
-                lstm_experiment(w, poge, alg, 30000 * poge.max_ep_len, early_stopping_acc=early_stopping_acc,
+                lstm_experiment(w, poge, alg, num_ep * poge.max_ep_len, early_stopping_acc=early_stopping_acc,
                                 verbose=verbose)
                 i += 1
-                print(f"Percentage of complete Stacked experiments: {round(i / num_runs, 2)}%")
+                print(f"Percentage of complete LSTM experiments: {round(i / num_runs, 2) * 100}%")
 
 
 if __name__ == '__main__':
-    run_poql_experiments()
-    # run_stacked_experiments()
-    # run_lstm_experiments()
+    # run_poql_experiments()
+    run_stacked_experiments()
+    run_lstm_experiments()
--- partially_observable_q_learning.py
+++ partially_observable_q_learning.py
@@ -485,11 +485,12 @@ def poql_experiment(exp_name, early_stopping_acc=1.01, model_type='mdp', verbose
                          env=env,
                          initial_sample_num=10000,
                          num_training_episodes=30000,
-                         update_interval=2000,
+                         update_interval=1000,
                          early_stopping_threshold=early_stopping_acc,
-                         freeze_after_ep=16000,
-                         re_init_epsilon=False,
-                         alergia_epsilon=0.005,
+                         freeze_after_ep=10000,
+                         re_init_epsilon=True,
+                         alergia_epsilon=0.05,
+                         initial_epsilon=0.4,
                          curiosity_reward=5,
                          curiosity_reward_reduction=0.99,
                          curiosity_rew_reduction_mode='mult',
@@ -685,14 +686,14 @@ def poql_experiment(exp_name, early_stopping_acc=1.01, model_type='mdp', verbose
                          env=env,
                          initial_sample_num=10000,
                          num_training_episodes=30000,
-                         update_interval=2000,
+                         update_interval=1000,
                          early_stopping_threshold=early_stopping_acc,
-                         freeze_after_ep=20000,
+                         freeze_after_ep=10000,
                          verbose=verbose,
                          test_episodes=100,
-                         initial_epsilon=0.9,
+                         initial_epsilon=0.4,
                          alergia_epsilon=0.05,
-                         re_init_epsilon=True,
+                         re_init_epsilon=False,
                          alergia_model_type=model_type,
                          curiosity_reward=5,
                          curiosity_reward_reduction=0.9,
@@ -719,4 +720,4 @@ def poql_experiment(exp_name, early_stopping_acc=1.01, model_type='mdp', verbose
 
 
 if __name__ == '__main__':
-    poql_experiment('simple_showcase2', early_stopping_acc=1, model_type='mdp')
+    poql_experiment('world1_confusing', early_stopping_acc=1, model_type='mdp')
